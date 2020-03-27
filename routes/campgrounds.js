c8bc3ba84b3acc44e6b5673b4ab4d6282aabb8e6
dpak-maurya <dpak.m932@gmail.com>
2020-03-27 10:21:04 +0530

    user associations

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,20 +13,26 @@ router.get("/", function(req, res) {
 });
 
 //Campgronds Create
-router.post("/", function(req, res) {
+router.post("/",isLoggedin, function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = { name: name, image: image, description: desc };
+  var author={
+    id:req.user._id,
+    username:req.user.username
+  }
+  var newCampground = { name: name, image: image, description: desc,author:author };
+  console.log(req.user);
   Campground.create(newCampground, function(err, camp) {
     if (err) console.log(err);
     else {
+      console.log(camp);
       res.redirect("/campgrounds");
     }
   });
 });
 //Campground New
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedin,function(req, res) {
   res.render("campgrounds/new");
 });
 
