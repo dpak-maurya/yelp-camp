82f977c1c96f6fbfeb373682adb3dc665a604335
dpak-maurya <dpak.m932@gmail.com>
2020-03-29 02:09:50 +0530

    adding env.ip

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use( "/campgrounds/:id/comments",commentRoutes);
 
-app.listen(process.env.PORT,function() {
+app.listen(process.env.PORT,process.env.IP,function() {
   console.log("server started");
 });
