048556464445ea83e098d67fe1bc48148c5c3c62
dpak-maurya <dpak.m932@gmail.com>
2020-03-29 02:07:20 +0530

    fixe scrolling background

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use( "/campgrounds/:id/comments",commentRoutes);
 
-app.listen(process.env.PORT,process.env.IP,function() {
+app.listen(process.env.PORT,function() {
   console.log("server started");
 });
