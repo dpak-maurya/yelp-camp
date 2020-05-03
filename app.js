964109c54cbbb752b272a4ffe0cc992defbf68bd
dpak-maurya <dpak.m932@gmail.com>
2020-05-03 16:32:46 +0530

    login page overflow remove

--- app.js
+++ app.js
@@ -1,67 +1,80 @@
-var express               = require("express"),
-    app                   = express(),
-    bodyParser            = require("body-parser"),
-    mongoose              = require("mongoose"),
-    moment                = require('moment'),
-    passport              = require("passport"),
-    LocalStrategy         = require("passport-local"),
-    User                  = require("./models/user"),
-    methodOverride        =require('method-override'),
-    flash                 =require('connect-flash'),
-    Campground            = require("./models/campground"),
-    Comment               = require("./models/comment"),
-    seedDB                = require("./seeds");
-    
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  moment = require("moment"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/user"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  seedDB = require("./seeds");
 
 //Requiring Routes
-var commentRoutes         =require('./routes/comments'),
-    campgroundRoutes      =require('./routes/campgrounds'),
-    indexRoutes           =require('./routes/index');
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
 // mongodb+srv://dpak:<password>@cluster0-nql1y.mongodb.net/test?retryWrites=true
-var url=process.env.DATABASEURL || "mongodb://localhost/YelpCamp";
-mongoose.connect(url.toString(),{
-  useNewUrlParser: true,
-  useUnifiedTopology:true,
-  useCreateIndex:true
-}).then(()=>{
-  console.log('Connected to DB!');
-}).catch(err=>{
-  console.log('ERROR:',err.message);
-});
+ var url = process.env.DATABASEURL || "mongodb://localhost/YelpCamp";
+mongoose
+  .connect(url.toString(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((err) => {
+    console.log("ERROR:", err.message);
+  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 //seedDB();
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(flash());
 
 app.use(
   require("express-session")({
     secret: "Corona virus has spread all 1over the world",
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
-);
+);app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function (req, res, next) {
+  app.locals.moment = moment;
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   app.locals.moment = moment;
   res.locals.currentUser = req.user;
-  res.locals.error=req.flash('error')
-  res.locals.success=req.flash('success');
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
   next();
 });
 
-app.use("/",indexRoutes);
-app.use("/campgrounds",campgroundRoutes);
-app.use( "/campgrounds/:id/comments",commentRoutes);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT,process.env.IP,function() {
+app.listen(process.env.PORT, function () {
   console.log("server started");
 });
